d9e589e044771dee9d13f46a4dbff531111fa3cb
Salman <salman666online@gmail.com>
2024-04-11 20:21:11 +0500

    fixed _getProposalType

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,14 +5,14 @@ import { deployProxy } from "../utils/deployment";
 async function deploy() {
   const [deployer] = await ethers.getSigners();
 
-  const createProposalFee = ethers.parseEther('5');
+  const createProposalFee = ethers.parseEther('1');
   const latestBlock = await ethers.provider.getBlock("latest");
   const startTimestamp = latestBlock!.timestamp + 300;
 
   console.log("Deploying from: ", deployer.address);
   console.log("Deploying DiamondDao contract");
 
-  const dao = await deployProxy("DiamondDao", [
+  const dao = await deployProxy("DiamondDaoTest", [
     "0x1000000000000000000000000000000000000001", // ValidatorSetHbbf
     "0x1100000000000000000000000000000000000001", // StakingHbbft
     "0x2000000000000000000000000000000000000001", // _reinsertPot
