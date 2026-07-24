fdb652e503320312f92c32b21a035546f3bec76f
Artsiom Vasilevich <gordn.freeman@proton.me>
2026-07-24 22:13:14 +0300

    fix: match dao pahse duration with version deployed to mainnet

--- contracts/DiamondDao.sol
+++ contracts/DiamondDao.sol
@@ -45,7 +45,7 @@ contract DiamondDao is IDiamondDao, Initializable, ReentrancyGuardUpgradeable, V
     /// @dev this is the duration of each DAO phase.
     /// A full DAO cycle consists of 2 phases: Proposal and Voting,
     /// therefore the full cycle duration is double that amount.
-    uint64 public constant DAO_PHASE_DURATION = 3 days;
+    uint64 public constant DAO_PHASE_DURATION = 7 days;
 
     address public reinsertPot;
     uint256 public createProposalFee;
